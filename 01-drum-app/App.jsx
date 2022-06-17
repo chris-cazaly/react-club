38ac365a09f78e192be200b6d61865caa32582d4
Chris Cazaly <chris.cazaly@coop.co.uk>
2022-06-17 15:57:32 +0100

    pass Audio source as a prop to <Button> component

--- 01-drum-app/App.jsx
+++ 01-drum-app/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import chime1 from './sounds/chime1.mp3'
+import coin1 from './sounds/coin1.mp3'
+import coin2 from './sounds/coin2.mp3'
+import coin3 from './sounds/coin3.mp3'
+import ding1 from './sounds/ding1.mp3'
+import ding2 from './sounds/ding2.mp3'
+import ding3 from './sounds/ding3.mp3'
+import ding4 from './sounds/ding4.mp3'
+
 
 import Button from './components/Button/Button.jsx';
 
 const App = () => {
   return (
     <>
-    <h1>test!</h1>
-      <Button text={'text'}/>
-      <Button text={'text1'}/>
+    <h1>Soundboard</h1>
+      <Button text={'chime'} audio={chime1}/>
+      <Button text={'coin1'} audio={coin1}/>
+      <Button text={'coin2'} audio={coin2}/>
+      <Button text={'coin3'} audio={coin3}/>
+      <Button text={'ding1'} audio={ding1}/>
+      <Button text={'ding2'} audio={ding2}/>
+      <Button text={'ding3'} audio={ding3}/>
+      <Button text={'ding4'} audio={ding4}/>
     </>
   )
 }
--- 01-drum-app/components/Button/Button.jsx
+++ 01-drum-app/components/Button/Button.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import sound from '../../sounds/chime1.mp3'
-// import testOther from "https://static.wikia.nocookie.net/leagueoflegends/images/7/7a/Kennen_Select.ogg/revision/latest?cb=20200711204115"
 
+const Button = ({text, audio}) => {
+  
+  const playSound = () => {
+    new Audio(audio).play();
+  }
 
-const Button = ({text}) => {
-    const audio = new Audio(sound)
-    const start = () => {
-      audio.play();
-    };
+  console.log(typeof(audio))
     
   return (
     <>
-    <button onClick={start}>{text}</button>
+      <button onClick={playSound}>{text}</button>
     </>
   )
 }
